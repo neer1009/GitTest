961d4fc49e404578bcf795a95d9ff96d7cdc94c8
neeraj <neeraj.gautam@walmart.com>
2015-11-30 22:36:54 +0530

    some new changes to Java

--- src/com/test/walmart/HelloWorld.java
+++ src/com/test/walmart/HelloWorld.java
@@ -5,6 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Next addition");
 		System.out.println("Hello Git !!");
+		// no file changes
+		callFunction();
+	}
+
+	private static void callFunction() {
+		System.out.println("Function got created");
+		
 	}
 
 }
